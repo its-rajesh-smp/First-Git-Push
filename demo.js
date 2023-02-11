067b7107e3fd7587119406f1698481304d16e121
its-rajesh-smp <its.rajeshsmp@gmail.com>
2023-02-11 17:58:58 +0530

    TASK 6 | DOM

--- demo.js
+++ demo.js
@@ -20,7 +20,25 @@ newList[0].style.border="1px solid rgba(0,0,0,.125)"
 newList[0].style.display="block"
 
 
-
-
+// QuerySelector
 var newList_tag=document.getElementsByTagName("li")
 newList_tag[4].style.fontWeight="bold"
+
+
+var secondItem=document.querySelector(".list-group-item:nth-child(2)")
+secondItem.style.backgroundColor="green"
+
+var thirdItem=document.querySelector(".list-group-item:nth-child(3)")
+secondItem.style.visibility="hidden"
+
+
+// QuerySelectorAll
+secondItem=document.querySelectorAll(".list-group-item")
+secondItem[1].style.color="green"
+
+
+for(let i=0;i<secondItem.length;i++){
+    if(i%2!=0){
+        secondItem[i].style.backgroundColor="green"
+    }
+}
