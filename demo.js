a7dd4f33173810f89e8037f441d36a1447756ac6
its-rajesh-smp <its.rajeshsmp@gmail.com>
2023-02-11 17:32:28 +0530

    DONE TASK 5 | DOM

--- demo.js
+++ demo.js
@@ -9,3 +9,18 @@ for(let i=0;i<document.getElementsByClassName("list-group-item").length;i++){
     document.getElementsByClassName("list-group-item")[i].style.fontWeight="bold"
 
 }
+
+
+var newList=document.getElementsByClassName("newClass")
+newList[0].style.paddingTop="0.75rem"
+newList[0].style.paddingBottom="0.75rem"
+newList[0].style.paddingLeft="1.25rem"
+newList[0].style.listStyle="none"
+newList[0].style.border="1px solid rgba(0,0,0,.125)"
+newList[0].style.display="block"
+
+
+
+
+var newList_tag=document.getElementsByTagName("li")
+newList_tag[4].style.fontWeight="bold"
