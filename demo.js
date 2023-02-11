0cb12a8692bde4ba1349b680c015eeadc0d4b55e
its-rajesh-smp <its.rajeshsmp@gmail.com>
2023-02-11 17:12:04 +0530

    Task Number 4 | DOM

--- demo.js
+++ demo.js
@@ -1,3 +1,11 @@
 document.getElementById('main-header').style.borderBottom="5px solid black"
 document.getElementsByClassName('title')[0].style.fontWeight="bold"
 document.getElementsByClassName('title')[0].style.color="green"
+
+
+
+document.getElementsByClassName("list-group-item")[2].style.backgroundColor="green"
+for(let i=0;i<document.getElementsByClassName("list-group-item").length;i++){
+    document.getElementsByClassName("list-group-item")[i].style.fontWeight="bold"
+
+}
